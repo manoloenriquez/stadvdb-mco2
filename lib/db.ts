2b696f18c94a462bb53ddc946445460d7b96bb1c
Manolo Enriquez <noloenriquez@gmail.com>
2022-02-09 18:09:48 +0800

    migrated to typescript

--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-anonymous-default-export */
 import mysql from 'mysql'
 import util from 'util'
 
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,7 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+
 import db from '../../lib/db'
 
-export default async function handler(req, res) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
     await db.query('CREATE DATABASE nextjsconnected')
     res.status(200).json({ msg: 'Created Database nextjsconnected' })
