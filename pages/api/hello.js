027e812cb2942beb333bcad85268fee5c318dc90
Manolo Enriquez <noloenriquez@gmail.com>
2022-02-09 15:15:12 +0800

    initialized mysql connection

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import db from '../../lib/db'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  try {
+    await db.query('CREATE DATABASE nextjsconnected')
+    res.status(200).json({ msg: 'Created Database nextjsconnected' })
+  } catch (err) {
+    await db.query('DROP DATABASE nextjsconnected')
+    res.status(200).json({ msg: 'Dropped Database nextjsconnected' })
+  }
 }
